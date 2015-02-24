8c498c286a701c59795e7c2f37ba428eae02fa60
Lara Urban <lara.urban@gmx.de>
2015-02-24 16:26:20 +0100

    Define hash and its keys;

--- fastaparser.pl
+++ fastaparser.pl
@@ -3,10 +3,12 @@
 use warnings;
 use strict;
 
+my %sequence=();
+
 my $filename="fasta.file";
 open FASTA,$filename or die "$filename $!";
 while (<FASTA>) 
 {
-    if ($_=~/^>/) {print "$_";}
+    if ($_=~/^>/) {$sequence{"$_"}="";}
 }
 close FASTA;
