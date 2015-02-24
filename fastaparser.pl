948beba86b9db4a4c049410bb3a36d41c8672f27
Frank Nierula <frank.nierula@stud-mail.uni-wuerzburg.de>
2015-02-24 17:03:55 +0100

    Committed program like formely advised

--- fastaparser.pl
+++ fastaparser.pl
@@ -3,11 +3,22 @@
 use warnings;
 use strict;
 
+my %sequence=();
+my $key="";
+
 my $filename="fasta.file";
-open, FASTA,$filename or die "filename $!";
-while <FASTA>
+open FASTA,$filename or die "filename $!";
+while (<FASTA>)
 {
-    print "$_";
+    if ($_=~/^>/)
+    {
+	$sequence{"$_"}="";
+	$key=$_;
+    }
+    print ($_)
+    else 
+{
+    $sequence{$key}=$sequence{$key}$_.
 }
 
 close FASTA
