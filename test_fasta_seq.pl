9c1e219e29f8572702537d95176bc33cb36ba9e3
Lara Urban <lara.urban@gmx.de>
2015-02-25 15:55:37 +0100

    Added new sub

--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Test::More tests=>2;
+use Test::More;
 
 require_ok("fasta_seq");
-
 my $ralph=new_ok("fasta_seq");
+can_ok("fasta_seq","ID");
+
+done_testing();
