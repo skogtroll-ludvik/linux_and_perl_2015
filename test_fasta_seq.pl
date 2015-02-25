9c4526435cad72f89c65f3a1ab3c72f8733f5b58
Lara Urban <lara.urban@gmx.de>
2015-02-25 16:18:46 +0100

    changed test file, sub set_ID is tested

--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -7,5 +7,7 @@ require_ok("fasta_seq");
 my $ralph=new_ok("fasta_seq");
 can_ok("fasta_seq","ID");
 is($ralph->ID,"ralph","get_ID");
+$ralph->ID("willi");
+is($ralph->ID,"willi","set_ID");
 
 done_testing();
