c79b9e169ddb2282df4ddcd7e439275faf4fbcef
Frank Nierula <frank.nierula@stud-mail.uni-wuerzburg.de>
2015-02-25 15:54:31 +0100

    Circumvented need for test number

--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -2,6 +2,10 @@
 
 use strict;
 use warnings;
-use Test::More tests => 1;
+use Test::More;
 
 require_ok('fasta_seq');
+my $ralph = new_ok("fasta_seq");
+can_ok("fasta_seq","ID");
+
+done_testing();
