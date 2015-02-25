56db35ddf07c5240d3d6e2aa63ded6b87936df67
Lara Urban <lara.urban@gmx.de>
2015-02-25 17:08:31 +0100

    Changed new_ok

--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -4,7 +4,7 @@ use warnings;
 use Test::More;
 
 require_ok("fasta_seq");
-my $ralph=new_ok("fasta_seq");
+my $ralph=new_ok("fasta_seq",["ralph"]);
 can_ok("fasta_seq","ID");
 is($ralph->ID,"ralph","get_ID");
 $ralph->ID("willi");
