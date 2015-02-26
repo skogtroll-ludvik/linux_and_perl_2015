875959b6094d31e19b05f0ebcabfb8628be83d39
Frank Nierula <frank.nierula@stud-mail.uni-wuerzburg.de>
2015-02-26 12:05:03 +0100

    Added 2 new tests (with species and without)

--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -5,14 +5,16 @@ use Test::More;
 
 
 require_ok("fasta_seq");
-my $ralph=new_ok("fasta_seq",["ralph","robot"]);
+my $ralph=new_ok("fasta_seq",["ralph"]);
 can_ok("fasta_seq","ID");
 is($ralph->ID,"ralph","get_ID");
 $ralph->ID("willi");
 is($ralph->ID,"willi","set_ID");
 
 can_ok("fasta_seq", "species");
-is($ralph->species,"robot","get_species");
+is($ralph->species,undef,"get_species");
+my $norbert=new_ok("fasta_seq",["norbert","robot"]);
+is($norbert->species,"robot","get_norbert_species");
 $ralph->species("human");
 is($ralph->species,"human","set_species");
 
