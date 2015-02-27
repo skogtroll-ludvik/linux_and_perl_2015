c466d06d492276b839c36aaa9d9947e3219bf365
Frank Nierula <frank.nierula@stud-mail.uni-wuerzburg.de>
2015-02-27 11:33:56 +0100

    Added test for default filename

--- test_fasta_parser.pl
+++ test_fasta_parser.pl
@@ -6,5 +6,5 @@ use Test::More;
 require_ok("fasta_parser");
 my $parser_obj = new_ok("fasta_parser");
 can_ok("fasta_parser","filename");
-
+is($parser_obj->filename, undef, "default filename");
 done_testing();
